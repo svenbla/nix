9810fcdfa561d807feb260e20379b99add739e83
svenbla <svenblahowsky@gmail.com>
2026-06-09 12:18:01 +0200

    fix google chat closing on cmd+w

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{pkgs, ...}:
 ###################################################################################
 #
 #  macOS's System configuration
@@ -10,20 +10,26 @@
 {
   system = {
     stateVersion = 5;
-    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+
     activationScripts.activateSettings.text = ''
-      # activateSettings -u will reload the settings from the database and apply them to the current session,
-      # so we do not need to logout and login again to make the changes take effect.
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
 
     defaults = {
-      menuExtraClock.Show24Hour = true; # show 24 hour clockmenuExtraClock
+      menuExtraClock.Show24Hour = true;
       NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+
+      CustomUserPreferences = {
+        "com.google.Chrome.app.pommaclcbfghclhalboakcipcmmndhcj" = {
+          NSUserKeyEquivalents = {
+            "Hide Google Chat" = "@w";
+
+            "Close Window" = "@~w";
+          };
+        };
+      };
     };
   };
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  # this is required if you want to use darwin's default shell - zsh
   programs.zsh.enable = true;
 }
